633c2551aa6af334cfbd358c973113c5e739d082
inbal <inbalamr@gmail.com>
2023-07-06 11:23:32 +0300

    url

--- production_app/tests.py
+++ production_app/tests.py
@@ -11,7 +11,7 @@ class Test_class(BaseClass):
         global driver
         log = self.log_conf()
         driver = setup
-        driver.get("http://52.87.216.192:5000/")
+        driver.get("http://100.27.10.149:5000/")
         sign_up = driver.find_element(By.CSS_SELECTOR, ".signup")
         sign_up.click()
         name = 'inbal'
