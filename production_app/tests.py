f547660947c951e704547b6c99831264d5c4fc01
inbal <inbalamr@gmail.com>
2023-07-12 17:37:01 +0300

    config test google url

--- production_app/tests.py
+++ production_app/tests.py
@@ -29,37 +29,56 @@
 #             raise AssertionError(msg)
 #         else:
 #             log.info("Test Passed successfully")
-
-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
+from selenium.webdriver.common.by import By
+import time
 import logging
 import os
 
-log = logging.getLogger('logfile')
+# Set up logging
+logging.basicConfig(filename='test.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
 
-@pytest.fixture(scope="module")
-def test_setup():
-    global driver
-    options = Options()
-    options.add_argument("--headless")  # Ensure GUI is off
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    
-    driver = webdriver.Chrome(options=options) # Using ChromeDriver instead of Firefox
-    driver.implicitly_wait(5)
-    driver.maximize_window()
-    yield
-    driver.close()
-    driver.quit()
-    log.info("Test Completed")
+class Test_class:
 
-def test_signup(test_setup):
-    driver.get("http://44.234.84.98:5000/signup")
-    driver.find_element_by_name("fullname").send_keys("test")
-    driver.find_element_by_name("email").send_keys("test@test.com")
-    driver.find_element_by_name("submit").click()
-    log.info("Signing up...")
+    @pytest.fixture(scope="module", autouse=True)
+    def setup(self):
+        global driver
+        options = Options()
+        options.add_argument("--headless")  # Ensure GUI is off
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
+        driver = webdriver.Chrome(options=options) # Using ChromeDriver instead of Firefox
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+        yield
+        driver.close()
+        driver.quit()
+        logging.info("Test Completed")
+
+    def test_signup(self, setup):
+        global driver
+        logging = self.setup()
+        driver = setup
+        driver.get("http://44.234.84.98:5000/")  # slave_1
+        sign_up = driver.find_element(By.CSS_SELECTOR, ".signup")
+        sign_up.click()
+        name = 'inbal'
+        user_name = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Full Name"]').send_keys(name)
+        email = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="E-Mail"]').send_keys('inbalamr@gmail.com')
+        sign_up_button = driver.find_element(By.CSS_SELECTOR, 'input[value="Sign-Up"]').click()
+        hello_user = driver.find_element(By.CSS_SELECTOR, '.helo').text
+        time.sleep(5)
+        try:
+            assert hello_user == f"Welcome {name}"
+        except AssertionError as msg:
+            logging.error(msg)
+            raise AssertionError(msg)
+        else:
+            logging.info("Test Passed successfully")
+
 
