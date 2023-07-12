b0ce8ca107896ec38add6d514b5059be48e2d9e9
inbal <inbalamr@gmail.com>
2023-07-12 16:00:04 +0300

    config test google url

--- production_app/config_test.py
+++ production_app/config_test.py
@@ -1,26 +1,17 @@
 import pytest
-from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-# @pytest.fixture(scope="class")
-# def setup():
-#     chrome_driver_path = ChromeDriverManager().install()
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")
-#     chrome_options.add_experimental_option("detach", True)
-#     service_obj = Service(chrome_driver_path)
-#     driver = webdriver.Chrome(service=service_obj, options=chrome_options)
-#     driver.implicitly_wait(5)
-#     return driver
+from selenium import webdriver
 
 
 @pytest.fixture(scope="class")
 def setup():
+    chrome_driver_path = ChromeDriverManager(version='latest').install()
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=Service(), options=chrome_options)
+    service_obj = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service_obj, options=chrome_options)
     driver.implicitly_wait(5)
     return driver
