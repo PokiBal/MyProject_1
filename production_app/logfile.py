5a032f64e93a8fe5a56a1b801e9c2259a0aaf8a4
inbal <inbalamr@gmail.com>
2023-07-13 16:18:23 +0300

    ly

--- production_app/logfile.py
+++ production_app/logfile.py
@@ -1,22 +1,23 @@
-# with open("logfile.log", "r") as f:
-#     last_line = f.readlines()[-1]
-#     last_line = last_line.rstrip('\n')  # remove new line characters
-#     entry = last_line.strip().split(":")
-#     if len(entry) < 5:
-#         print("Unexpected log format.")
-#         timestamp = "Unknown"
-#         message = "Unknown"
-#     else:
-#         timestamp = ":".join(entry[:2])
-#         message = entry[4].strip().replace('"', '\\"')
-
-#print(f"{timestamp}, {message}")
-
 with open("logfile.log", "r") as f:
     last_line = f.readlines()[-1]
-    entry = last_line.strip().split(":")
-    timestamp = entry[0] + ":" + entry[1]
-    message = entry[4].strip().replace('"', '\\"')
+    last_line = last_line.rstrip('\n')  # remove new line characters
+    entry = last_line.strip().split(" - ")
+    if len(entry) < 4:
+        print("Unexpected log format.")
+        timestamp = "Unknown"
+        message = "test"
+    else:
+        timestamp = entry[0]
+        message = entry[3].strip().replace('"', '\\"')
 
 print(f"{timestamp}, {message}")
+
+
+# with open("logfile.log", "r") as f:
+#     last_line = f.readlines()[-1]
+#     entry = last_line.strip().split(":")
+#     timestamp = entry[0] + ":" + entry[1]
+#     message = entry[4].strip().replace('"', '\\"')
+
+# print(f"{timestamp}, {message}")
 
