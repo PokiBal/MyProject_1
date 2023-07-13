3634f404a157a139fa9406bfb15dd32bc188d8ac
inbal <inbalamr@gmail.com>
2023-07-13 15:58:41 +0300

    jenkinsfile and logfile.py

--- production_app/logfile.py
+++ production_app/logfile.py
@@ -1,5 +1,6 @@
 with open("logfile.log", "r") as f:
     last_line = f.readlines()[-1]
+    last_line = last_line.rstrip('\n')  # remove new line characters
     entry = last_line.strip().split(":")
     if len(entry) < 5:
         print("Unexpected log format.")
@@ -10,3 +11,4 @@ with open("logfile.log", "r") as f:
         message = entry[4].strip().replace('"', '\\"')
 
 print(f"{timestamp}, {message}")
+s
