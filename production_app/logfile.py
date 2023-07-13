1d6a2f98fbc04c51ea458eac8e4cc5523b9608a8
inbal <inbalamr@gmail.com>
2023-07-13 16:11:34 +0300

    ly

--- production_app/logfile.py
+++ production_app/logfile.py
@@ -1,14 +1,22 @@
+# with open("logfile.log", "r") as f:
+#     last_line = f.readlines()[-1]
+#     last_line = last_line.rstrip('\n')  # remove new line characters
+#     entry = last_line.strip().split(":")
+#     if len(entry) < 5:
+#         print("Unexpected log format.")
+#         timestamp = "Unknown"
+#         message = "Unknown"
+#     else:
+#         timestamp = ":".join(entry[:2])
+#         message = entry[4].strip().replace('"', '\\"')
+
+#print(f"{timestamp}, {message}")
+
 with open("logfile.log", "r") as f:
     last_line = f.readlines()[-1]
-    last_line = last_line.rstrip('\n')  # remove new line characters
     entry = last_line.strip().split(":")
-    if len(entry) < 5:
-        print("Unexpected log format.")
-        timestamp = "Unknown"
-        message = "Unknown"
-    else:
-        timestamp = ":".join(entry[:2])
-        message = entry[4].strip().replace('"', '\\"')
+    timestamp = entry[0] + ":" + entry[1]
+    message = entry[4].strip().replace('"', '\\"')
 
 print(f"{timestamp}, {message}")
 
