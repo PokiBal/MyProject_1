5341cb0f8a7340ac072e31648bdc26a5206012f8
inbal <inbalamr@gmail.com>
2023-07-13 14:00:13 +0300

    update jenkins file and logfile

--- production_app/logfile.py
+++ production_app/logfile.py
@@ -1,25 +1,9 @@
-# with open("logfile.log", "r") as f:
-#     last_line = f.readlines()[-1]
-#     entry = last_line.strip().split(":")
-#     timestamp = entry[0] + ":" + entry[1]
-#     message = entry[4].strip().replace('"', '\\"')
-
-# print(f"{timestamp}, {message}")
-
 import logging
 
-def setup_logger(name, log_file, level=logging.INFO):
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+with open("logfile.log", "r") as f:
+    last_line = f.readlines()[-1]
+    entry = last_line.strip().split(":")
+    timestamp = ":".join(entry[:2])
+    message = entry[4].strip().replace('"', '\\"')
 
-    handler = logging.FileHandler(log_file)        
-    handler.setFormatter(formatter)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(handler)
-
-    return logger
-
-# For example, setup logger with name 'db'
-logger = setup_logger('db', 'db.log')
-logger.info('This is a log message')
+print(f"{timestamp}, {message}")
