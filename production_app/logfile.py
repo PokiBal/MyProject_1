6fb86a18296fbb46306c00d95cedb56b27518650
inbal <inbalamr@gmail.com>
2023-07-13 15:42:49 +0300

    logfile

--- production_app/logfile.py
+++ production_app/logfile.py
@@ -1,9 +1,12 @@
-import logging
-
 with open("logfile.log", "r") as f:
     last_line = f.readlines()[-1]
     entry = last_line.strip().split(":")
-    timestamp = ":".join(entry[:2])
-    message = entry[4].strip().replace('"', '\\"')
+    if len(entry) < 5:
+        print("Unexpected log format.")
+        timestamp = "Unknown"
+        message = "Unknown"
+    else:
+        timestamp = ":".join(entry[:2])
+        message = entry[4].strip().replace('"', '\\"')
 
 print(f"{timestamp}, {message}")
